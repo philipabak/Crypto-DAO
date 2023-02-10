8c014f315f17373315a08cf884811abb108da35a
Philip <philipkarlson77@gmail.com>
2023-02-10 09:23:43 +0200

    DAO

---
+++ backend/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x388600ED19Fb46C43284fBB8bc2d28ccd87b2D13";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
